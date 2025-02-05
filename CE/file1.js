8e68fe62969667eeafe23fba23179da9ea0e13cd
simplehacgithub <tanyach082020@gmail.com>
2025-02-05 09:45:34 +0530

    new

--- CE/file1.js
+++ CE/file1.js
@@ -1,3 +1,6 @@
-const fs = require('fs');
-const data = fs.readFileSync("./data.txt");
-console.log(data.toString())
+ const fs = require('fs');
+// const data = fs.readFileSync("./data.txt");
+// console.log(data.toString())
+fs.writeFileSync("./data.txt","ce promises");
+fs.appendFileSync("./data.txt","aktu")
+fs.renameSync("./data.txt","data2.txt")
