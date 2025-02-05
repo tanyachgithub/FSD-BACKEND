a25396882c8a6066414ecad4ad1fd42a15b20c87
simplehacgithub <tanyach082020@gmail.com>
2025-02-05 10:00:29 +0530

    5/2/25

--- CE/file1.js
+++ CE/file1.js
@@ -1,6 +1,11 @@
  const fs = require('fs');
-// const data = fs.readFileSync("./data.txt");
-// console.log(data.toString())
-fs.writeFileSync("./data.txt","ce promises");
-fs.appendFileSync("./data.txt","aktu")
-fs.renameSync("./data.txt","data2.txt")
+ const data = fs.readFileSync("./data.txt","utf-8");
+ console.log(data)
+//fs.writeFileSync("./data.txt","ce promises");
+// fs.appendFileSync("./data.txt","aktu")
+// fs.renameSync("./data.txt","data2.txt")
+if (data.match("H")) {
+    console.log("H is present");
+    const changedata = data.replace("H", "ABES");
+    fs.writeFileSync("./data.txt",changedata);
+}
